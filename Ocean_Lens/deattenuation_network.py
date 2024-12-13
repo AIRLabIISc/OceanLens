c43837f71325ff8d5499fc856c7ac188f1cb6773
Peace <137488762+dhatri2003@users.noreply.github.com>
2024-12-13 10:20:39 +0530

    Update deattenuation_network.py

--- Ocean_Lens/deattenuation_network.py
+++ Ocean_Lens/deattenuation_network.py
@@ -6,15 +6,13 @@ class DeattenuateNet(nn.Module):
     def __init__(self):
         super().__init__()
         # Original attenuation convolution layer
-        self.deattenuation_conv = nn.Conv2d(1, 6, 1, bias=False)
-        nn.init.uniform_(self.deattenuation_conv.weight, 0, 5)
+        self.attenuation_conv = nn.Conv2d(1, 6, 1, bias=False)
+        nn.init.uniform_(self.attenuation_conv.weight, 0, 5)
         
         # New convolution layers
         self.conv1 = nn.Conv2d(6, 12, kernel_size=3, stride=1, padding=1)
-        #self.conv2 = nn.Conv2d(12, 12, kernel_size=3, stride=1, padding=1)
-        #self.conv3 = nn.Conv2d(12, 12, kernel_size=3, stride=1, padding=1)  # Third convolution layer
-        #self.conv4 = nn.Conv2d(12, 12, kernel_size=3, stride=1, padding=1)  # Fourth convolution layer
-        #self.conv5 = nn.Conv2d(12, 6, kernel_size=3, stride=1, padding=1)   # Fifth convolution layer
+        self.conv2 = nn.Conv2d(12, 6, kernel_size=3, stride=1, padding=1)
+        
 
         self.a_f = nn.Parameter(torch.rand(6, 1, 1))
         self.relu = nn.ReLU()
@@ -27,24 +25,21 @@ class DeattenuateNet(nn.Module):
 
     def forward(self, I_D, depth):
         # Pass depth through attenuation convolution layer
-        deattn_conv = torch.exp(-self.relu(self.deattenuation_conv(depth)))
+        deattn_conv = torch.exp(-self.relu(self.attenuation_conv(depth)))
         
         # Pass through additional convolution layers
         deattn_conv = self.relu(self.conv1(deattn_conv))
-        #deattn_conv = self.relu(self.conv2(deattn_conv))
-        #deattn_conv = self.relu(self.conv3(deattn_conv))  # Third convolution layer
-        #deattn_conv = self.relu(self.conv4(deattn_conv))  # Fourth convolution layer
-        #deattn_conv = self.relu(self.conv5(deattn_conv))  # Fifth convolution layer
-        
+        deattn_conv = self.relu(self.conv2(deattn_conv))
+    
         # Calculate b_d for attenuation
-        b_d = torch.stack(tuple(
-            torch.sum(deattn_conv[:, i:i + 2, :, :] * self.relu(self.a_f[i:i + 2]), dim=1) for i in
-            range(0, 6, 2)), dim=1)
+        # b_d = torch.stack(tuple(
+        #     torch.sum(deattn_conv[:, i:i + 3, :, :] * self.relu(self.a_f[i:i + 3]), dim=1) for i in
+        #     range(0, 6, 2)), dim=1)
         
         # Calculate b_d for attenuation -- changing number of exponentials
-        #b_d = torch.stack(tuple(
-         #   torch.sum(attn_conv[:, i:i + 3, :, :] * self.relu(self.a_f[i:i +3 ]), dim=1) for i in
-         #   range(0, 6, 2)), dim=1) #3 exponentials
+        b_d = torch.stack(tuple(
+           torch.sum(deattn_conv[:, i:i + 2, :, :] * self.relu(self.a_f[i:i + 2 ]), dim=1) for i in
+           range(0, 6, 2)), dim=1) #3 exponentials
         
 
         correction_factor = torch.exp(torch.clamp(b_d * depth, 0, float(torch.log(torch.tensor([3.])))))
@@ -65,3 +60,6 @@ class DeattenuateNet(nn.Module):
 
 
 
+
+
+    
